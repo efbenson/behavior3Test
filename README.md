a2b1d31e2fd7f57baf12b19f3a4873b448924997
Erik Benson <efbenson@gmail.com>
2016-08-13 22:46:04 -0400

    feat: commits initial code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # behavior3Test
+
+## A simple example on how to implement the behavior3 lib in nodejs (and similarly in the browser)
+
+Note: the behavior3 lib does not support nodejs currently so there is an included version of it in the lib folder that works with node.
+
+This is a simple behavior tree built off the example given in the great article by Chris Simpson on behavior trees [here](http://www.gamasutra.com/blogs/ChrisSimpson/20140717/221339/Behavior_trees_for_AI_How_they_work.php)
+
+Download the sample and run node app and you will get an output
+
+[https://github.com/behavior3/behavior3js](https://github.com/behavior3/behavior3js)
--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 'use strict';
 
+var b3 = require('./lib/behavior3js').b3;
+
 var treeLoader = require('./lib/treeLoader');
 
 treeLoader.init();
+
+var ai = treeLoader.ai();
+
+console.log('**** Lucky tries the door');
+var lucky = {
+    memory:  new b3.Blackboard()
+};
+lucky.memory.set('name', 'Lucky');
+ai.guy.tick(lucky, lucky.memory);
+
+console.log('');
+console.log('**** Thief tries the door');
+var thief = {
+    memory:  new b3.Blackboard()
+};
+thief.memory.set('name', 'Thief');
+thief.memory.set('locked', true);
+thief.memory.set('lockpick-level', 8);
+
+ai.guy.tick(thief, thief.memory);
+
+console.log('');
+console.log('**** Thug tries the door');
+var thug = {
+    memory:  new b3.Blackboard()
+};
+thug.memory.set('name', 'Thug');
+thug.memory.set('locked', true);
+thug.memory.set('lockpick-level', 2);
+
+ai.guy.tick(thug, thug.memory);
+console.log('');
+console.log('');
+console.log('**** Simulation complete');
